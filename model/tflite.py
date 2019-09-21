3551a1550648db8ebd4f563a3f8d44e96ed505e6
Bernardo <bernardogcgalvao@gmail.com>
2019-09-21 13:06:38 +0100

    added support for vanilla tf model (protobuf frozen graph format)

--- model/tflite.py
+++ model/tflite.py
@@ -11,67 +11,66 @@ def filter_row(row):
         return int(row.split(': ')[-1]) - 1
         # look out! label mismatch may happen from this - 1
     elif 'name' in row:
-        return row.split(': ')[-1].replace(
-            "'",
-            ""
-        ).strip("\n")
+        return row.split(': ')[-1].replace("'", "").strip("\n")
 
 
 def load_labels(labels_path):
     # initialize the labels dictionary
     print("[INFO] parsing class labels...")
-    
+
     labels = [
-        filter_row(row)
-        for row in open(labels_path)
+        filter_row(row) for row in open(labels_path)
         if 'id' in row or 'name' in row
     ]
 
-    return dict(zip(*[iter(labels)]*2))
+    return dict(zip(*[iter(labels)] * 2))
 
 
-class SSD_Model:
+class SSD_TFLite:
     """
     This class makes the heavy assumption that you're using one of the SSD models
     from TensorFlow Object Detection API.
     """
-
-    def __init__(self, model_path='detect.tflite', labels_path='labels_map.pbtxt'):
+    def __init__(self,
+                 model_path='detect.tflite',
+                 labels_path='labels_map.pbtxt'):
         # Load TFLite model and allocate tensors.
         self.interpreter = tf.lite.Interpreter(model_path=model_path)
         self.interpreter.allocate_tensors()
-        
+
         self.output_details = self.interpreter.get_output_details()
         self.input_details = self.interpreter.get_input_details()
-        
+
         self.labels = load_labels(labels_path)
-        self.input_shape = self.input_details[0]['shape'][[1,2]]
+        self.input_shape = self.input_details[0]['shape'][[1, 2]]
         self.width = self.input_shape[0]
         self.height = self.input_shape[1]
-
 
     def fprop(self, input_data):
         # check shape
         # (should also check if f32)
         #assert input_data.shape == self.input_details[0]['shape'], 'welp'
-        
+
         # set input tensor and forward prop
         #print(input_data.shape)
-        self.interpreter.set_tensor(self.input_details[0]['index'], input_data.astype('float32'))
+        self.interpreter.set_tensor(self.input_details[0]['index'],
+                                    input_data.astype('float32'))
         self.interpreter.invoke()
 
         # update refs to output tensors
         self.boxes, self.classes, self.scores, self.num_detections = [
-            self.interpreter.get_tensor(output['index']) for output in self.output_details
+            self.interpreter.get_tensor(output['index'])
+            for output in self.output_details
         ]
         # change int classes to their labels
         #self.classes = list(map(self.labels.get, self.classes))
-        self.classes = np.array([self.labels[key] for key in self.classes.reshape([-1]).astype(int)])
+        self.classes = np.array([
+            self.labels[key] for key in self.classes.reshape([-1]).astype(int)
+        ])
         # reshape for convenience
         self.boxes = self.boxes.reshape([-1, 4])
         self.scores = self.scores.reshape([-1])
         return self
-
 
     def get_output_tensors(self, threshold=None):
         """
@@ -80,7 +79,7 @@ class SSD_Model:
         """
         assert (type(threshold) is float) and (threshold >= 0.0) \
             and (threshold <=1.0)
-        
+
         if threshold is None:
             return self.boxes, self.classes, self.scores, self.num_detections
         else:
@@ -91,12 +90,13 @@ class SSD_Model:
             # print(self.boxes[idx])
             # print(self.classes[idx])
             # print(self.scores[idx])
-            return self.boxes[idx], self.classes[idx], self.scores[idx], idx.size
+            return self.boxes[idx], self.classes[idx], self.scores[
+                idx], idx.size
             # for debugging purposes
             #return [self.boxes[0]], [self.classes[0]], [self.scores[0]], 1
 
 
 if __name__ == '__main__':
 
-    model = SSD_Model('./tf_files/detect.tflite', './tf_files/label_map.pbtxt')
-    
+    model = SSD_TFLite('./tf_files/detect.tflite',
+                       './tf_files/label_map.pbtxt')
--- tflite_cam_detect.py
+++ tflite_cam_detect.py
@@ -1,27 +1,29 @@
 import argparse
-from model.ssd import SSD_Model
+from model.tflite import SSD_TFLite
 from video.streamer import Streamer
 import cv2
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument(
-    "-m", "--model", default='./tf_files/detect.tflite',
-    help="path to TensorFlow Lite object detection model"
-)
-ap.add_argument(
-    "-l", "--labels", default='./tf_files/label_map.pbtxt',
-    help="path to labels file"
-)
-ap.add_argument(
-    "-c", "--confidence", type=float, default=0.1,
-    help="minimum probability to filter weak detections"
-)
-ap.add_argument(
-    "-d", "--detection_engine", type=str, default="lite",
-    help="pick a selection engine: (default) 'lite' to use \
-    tf.lite.Interpreter; 'edge' to use Coral's Edge TPU."
-)
+ap.add_argument("-m",
+                "--model",
+                default='./tf_files/3/detect.tflite',
+                help="path to TensorFlow Lite object detection model")
+ap.add_argument("-l",
+                "--labels",
+                default='./tf_files/3/label_map.pbtxt',
+                help="path to labels file")
+ap.add_argument("-c",
+                "--confidence",
+                type=float,
+                default=0.1,
+                help="minimum probability to filter weak detections")
+ap.add_argument("-d",
+                "--detection_engine",
+                type=str,
+                default="lite",
+                help="pick a selection engine: (default) 'lite' to use \
+    tf.lite.Interpreter; 'edge' to use Coral's Edge TPU.")
 args = vars(ap.parse_args())
 # I hereby convene that command line args are prefixed with _ and uppercased
 _MODEL_PATH = args['model']
@@ -34,38 +36,29 @@ _DETECTION_ENGINE = args['detection_engine']
 
 print(_MODEL_PATH)
 
-model = SSD_Model(
-    model_path=_MODEL_PATH,
-    labels_path=_LABELMAP_PATH
-)
+model = SSD_TFLite(model_path=_MODEL_PATH, labels_path=_LABELMAP_PATH)
 
 print("[INFO] min confidence", _MIN_CONFIDENCE)
 
 # initialize the video stream and allow the camera sensor to warmup
 print("[INFO] starting video stream...")
 video_stream = Streamer()
-video_stream.set_detection_model(
-    model,
-    view_mode='camera',
-    nn_input_mode='zero-pad'
-)
-
-
-
+video_stream.set_detection_model(model,
+                                 view_mode='camera',
+                                 nn_input_mode='zero-pad')
 
 # loop over the frames from the video stream
 while True:
-    
+
     # there's stuff to work on according to this note:
     # https://www.tensorflow.org/lite/models/object_detection/overview#location
     bgr_frame, rgb_frame = video_stream.next_frame()
     boxes, classes, scores, num_detections = model.fprop(
-        rgb_frame
-    ).get_output_tensors(_MIN_CONFIDENCE)
+        rgb_frame).get_output_tensors(_MIN_CONFIDENCE)
     boxes = video_stream.boxes2bbs(boxes)
     results = zip(boxes, classes, scores)
 
-    # loop over the results https://58surf.com/pt/pt/catalogsearch/result/?q=catch
+    # loop over the results
     for box, label, score in results:
         # extract the bounding box and box and predicted class label
         # box = r.bounding_box.flatten().astype("int")
@@ -82,23 +75,16 @@ while True:
         #     raise
         # #assert box[0][1] < box[1][1]
         cv2.rectangle(bgr_frame, box[1], box[0], (0, 255, 0))
-        
+
         # label the rectangle
         y = start_y - 15 if start_y - 15 > 15 else start_y + 15
         #text = "{} :: {:.2f}%".format(label, score * 100)
         text = "{:.2f}%".format(score * 100)
         #print(start_x, y)
-        
-        cv2.putText(
-            bgr_frame, text, (start_x, y), cv2.FONT_HERSHEY_SIMPLEX,
-            .35, (0, 255, 0), 2
-        )
-        
-        # cv2.putText(
-        #     bgr_frame, text, (start_x, y),
-        #     cv2.FONT_HERSHEY_SIMPLEX, 
-        #     int(1), (0, 255, 0)#, int(2)
-        # )
+
+        cv2.putText(bgr_frame, text, (start_x, y), cv2.FONT_HERSHEY_SIMPLEX,
+                    .35, (0, 255, 0), 2)
+
 
     # show the output frame and wait for a key press
     cv2.imshow("Frame", bgr_frame)
@@ -107,4 +93,3 @@ while True:
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
-
