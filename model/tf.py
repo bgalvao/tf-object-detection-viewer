5036a364e13b9f9c8b24062afa73d0dc90e8dd71
Bernardo <bernardogcgalvao@gmail.com>
2019-10-16 13:49:39 +0100

    tf cam detect :: better bounding box viz

--- model/tf.py
+++ model/tf.py
@@ -35,6 +35,11 @@ class SSD_TF:
         
         # shape could pertain to (batch size, "channels", n_dtections, features)
         self.output_tensor = self.net.forward() # (1, 1, 100, 7)
+        # each line in this tensor seems to follow
+        # (??, class_idx, confidence, *(coordinates))
+
+        def class_idx_mapper(farray):
+            return farray[1]
 
         def score_mapper(farray):
             return farray[2]
@@ -43,38 +48,68 @@ class SSD_TF:
             # (xmin, ymin, xmax, ymax)
             return farray[3], farray[4], farray[5], farray[6]
 
-        # TODO: find where predicted class lays in this tensor    
+        
         self.out_scores = np.array(
             list(map(score_mapper, self.output_tensor[0,0]))
         )
         self.bouding_boxes = np.array(
             list(map(bb_mapper, self.output_tensor[0,0]))
         ).reshape((-1, 4))
+
+        self.class_idxs = np.array(
+            list(map(class_idx_mapper, self.output_tensor[0,0]))
+        ).astype(int)
         return self
 
 
     def filter_results(self, min_prob=.8):
         self.idx = np.argwhere(self.out_scores > min_prob)
 
-
     def get_bounding_boxes(self):
         return self.bouding_boxes[self.idx]
-
 
     def get_scores(self):
         return self.out_scores[self.idx]
 
+    def get_class_idxs(self):
+        return self.class_idxs[self.idx]
+
 
 if __name__ == '__main__':
-    img = cv2.imread(filename='./tf_files/sample_a.jpg')
+
+    from os.path import isfile
+
+    img_filepath = './samples/sample_b.jpg'
+
+    if not isfile(img_filepath):
+        print('image not found ::', img_filepath)
+        exit(-2)
+
+
+    colormap = {
+        'alice_blue': (240,248,255),
+        'alien_armpit': (132,222,2),
+        'amber': (255,126,0),
+        'amarath_pink': (241,156,187),
+        'fuchsia': (255,0,255)
+    }
+    colors = {i: color for i, color in enumerate(colormap.values())}
+
+
+    img = cv2.imread(filename=img_filepath)
+    
+
     height, width = list(img.shape)[:2]
 
     model = SSD_TF()
     model.fprop(img)
+
     idx = np.argwhere(model.out_scores > .8)  # min thresh
     bbs = model.bouding_boxes[idx]
+    scores = model.out_scores[idx]
+    class_idxs = model.class_idxs[idx]#.tolist()
 
-    for bb in bbs:
+    for bb, score, class_idx in zip(bbs, scores, class_idxs):
         xmin, ymin, xmax, ymax = bb.reshape(4)
 
         xmin = int(xmin * width)
@@ -83,37 +118,36 @@ if __name__ == '__main__':
         ymin = int(ymin * height)
         ymax = int(ymax * height)
 
+        score = score[0]
+        class_idx = class_idx[0]
+
         cv2.rectangle(
             img,
             (xmin, ymin),
             (xmax, ymax),
-            (0, 225, 0),
-            thickness=2
+            colors[class_idx - 1],
+            #thickness=2
         )
-            
+
+        # label the rectangle
+        vert_offset = 3
+        y = ymin - vert_offset \
+            if ymin - vert_offset > vert_offset \
+            else ymin + vert_offset
+        #text = "{} :: {:.2f}%".format(label, score * 100)
+        text = "class [{}] :: {:.0f}%".format(class_idx, score * 100)
+        #print(start_x, y)
+
+        cv2.putText(
+            img,
+            text,
+            (xmin, y),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            .35,
+            colors[class_idx - 1], 1
+        )
+
+
     cv2.imshow('Image', img)
     cv2.waitKey()
     cv2.destroyAllWindows()
-
-
-# tensorflowNet.setInput(cv2.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
-
-# # Runs a forward pass to compute the net output
-# networkOutput = tensorflowNet.forward()
-
-# # Loop on the outputs
-# for detection in networkOutput[0,0]:
-    
-#     score = float(detection[2])
-#     if score > 0.2:
-    	
-#         left = detection[3] * cols
-#         top = detection[4] * rows
-#         right = detection[5] * cols
-#         bottom = detection[6] * rows
-
-#         #draw a red rectangle around detected objects
-#         cv2.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (0, 0, 255), thickness=2)
-
-# # Show the image with a rectagle surrounding the detected objects 
-
--- tf_cam_detect.py
+++ tf_cam_detect.py
@@ -1,10 +1,16 @@
 import argparse
 from model.tf import SSD_TF
+from model.tflite import load_labels
 from video.streamer import Streamer
 import cv2
+from os.path import isfile
+
+from colormap import colormap
+colors = {i: color for i, color in enumerate(colormap.values())}
 
 
-RELEASE = 'bcst/'
+RELEASE = 'bwdst_filtered_2000/'
+
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -19,6 +25,11 @@ ap.add_argument("-c",
                 default='./tf_files/' + RELEASE + 'graph.pbtxt',
                 help="path to pbtxt graph config file")
 
+ap.add_argument("-l",
+                "--label_map",
+                default='./tf_files/' + RELEASE + 'label_map.pbtxt',
+                help="label map file path")
+
 ap.add_argument("-p",
                 "--min_prob",
                 type=float,
@@ -31,6 +42,14 @@ args = vars(ap.parse_args())
 _GRAPH_PB_PATH = args['graph_pb']
 _GRAPH_CONFIG_PATH = args['graph_config']
 _MIN_CONFIDENCE = args['min_prob']
+_LABELMAP_PATH = args['label_map']
+
+if not isfile(_GRAPH_PB_PATH):
+    raise
+elif not isfile(_GRAPH_CONFIG_PATH):
+    raise
+elif not isfile(_LABELMAP_PATH):
+    raise
 
 print("[INFO] min confidence", _MIN_CONFIDENCE)
 
@@ -54,7 +73,8 @@ def bb_mapper(bb):
     return (xmin, ymin), (xmax, ymax)
 
 
-i = 0
+labels = load_labels(_LABELMAP_PATH)
+
 
 # loop over the frames from the video stream
 while True:
@@ -64,15 +84,31 @@ while True:
     model.fprop(bgr_frame)
     model.filter_results(_MIN_CONFIDENCE)
     boxes = map(bb_mapper, model.get_bounding_boxes())
+    scores = model.get_scores()
+    class_idxs = model.get_class_idxs() - 1  # ja ajusta o indice
 
-    if i % 15 == 0:
-        print('\n{} detections'.format(model.get_bounding_boxes().shape[0]))
-        for j in model.get_scores().round(2):
-            print('-', j)
+
+    # if i % 15 == 0:
+    #     print('\n{} detections'.format(model.get_bounding_boxes().shape[0]))
+    #     for j in model.get_scores().round(2):
+    #         print('-', j)
 
     # loop over the resulting bounding boxes
-    for box in boxes:
-        cv2.rectangle(bgr_frame, box[0], box[1], (0, 255, 0), 2)
+    for box, score, class_idx in zip(boxes, scores, class_idxs):
+        color = colors[class_idx]
+        cv2.rectangle(bgr_frame, box[0], box[1], color, 2)
+
+        # label the rectangle
+        label = labels[class_idx]
+        vert_offset = 3; y = start_y - vert_offset \
+            if start_y - vert_offset > vert_offset \
+            else start_y + vert_offset
+
+        text = "{} :: {:.0f}%".format(labels[label_idx], score * 100)
+        cv2.putText(
+            bgr_frame, text, (box[0][0], y), cv2.FONT_HERSHEY_SIMPLEX,
+            .35, 1
+        )
 
     # show the output frame and wait for a key press
     cv2.imshow("Frame", bgr_frame)
@@ -81,5 +117,3 @@ while True:
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
-
-    i += 1
